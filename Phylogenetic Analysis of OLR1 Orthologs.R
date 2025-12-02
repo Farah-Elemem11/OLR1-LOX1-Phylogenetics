939c9d08a5460b0de7d6a9b4bc4d534a8a2957aa
Farah-Elemem11 <farahelemam2111@gmail.com>
2025-12-02 03:01:53 +0200

    Add files via upload

---
+++ Phylogenetic Analysis of OLR1 Orthologs.R
@@ -0,0 +1,39 @@
+# =======================================================================
+# Phylogenetic Analysis of OLR1 Orthologs
+# - Multiple Sequence Alignment (MSA)
+# - Pairwise Identity Distance
+# - Neighbor-Joining Phylogenetic Tree
+# ======================================================================
+
+# Load required library for Multiple Sequence Alignment (MSA)
+library(msa)
+# 1) Read the FASTA file containing OLR1 sequences
+mysequences <- readAAStringSet(mySequenceFile)
+print(head(mysequences))
+# 2) Perform Multiple Sequence Alignment (MSA)
+myfirstalingment =msa(mysequences)
+# Print the complete alignment to see the result
+print(myfirstalingment , show  ="complete")
+# 3) Save the alignment as a PDF with formatting
+msaPrettyPrint(myfirstalingment,
+               output = "pdf", showNames = "left",
+               showNumbering = "right", showConsensus = "bottom",
+               askForOverwrite = FALSE , showLogo = "none",
+               shadingColors = "blue" , shadingMode = "similar",
+               file = "alignment.pdf")
+# 4) Convert the alignment to seqinr format for distance calculation
+OLR1_aln2 = msaConvert(myfirstalingment ,type = "seqinr::alignment")
+# Load seqinr library for calculating sequence distances
+library(seqinr)
+# 5) Calculate pairwise distances using identity
+d = dist.alignment(OLR1_aln2  ,"identity")
+# Display the distance matrix
+as.matrix(d , drop =F)
+d
+# Load ape library for phylogenetic tree construction
+library(ape)
+# Load required library for Multiple Sequence Alignment (MSA)
+OLR1_Tree= nj(d)
+# 7) Plot the phylogenetic tree
+plot(OLR1_Tree , main = "Phylogenetic Tree of OLR1 Sequences")
+
